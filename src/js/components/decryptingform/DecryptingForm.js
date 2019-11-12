56405dab6e68200eb132d00cfed02d23ddf16929
Sondre Bye Humberset <sondrebh99@gmail.com>
2019-11-13 00:21:44 +0100

    add live decryption

--- src/js/components/decryptingform/DecryptingForm.js
+++ src/js/components/decryptingform/DecryptingForm.js
@@ -1,5 +1,5 @@
 // Contrib
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 
 // Custom
 import { ciphEncrypt, ciphDecrypt } from '../../helpers/helpers'
@@ -11,17 +11,41 @@ const DecryptingForm = props => {
 
     const { state, dispatch } = useContext(AppContext);
 
-    useEffect(() => {
-        ciphDecrypt(localStorage.getItem('state').split(','), state.masterkey).then(str => {
-            dispatch( { type: 'LOAD_STATE_COMPLETE', str } );
+    const [ localState, setLocalState ] = useState({ str: '', index: 0 });
+
+    const encryptedSavedStateArr = localStorage.getItem('state').split(',');
+
+    const decrypt = index => {
+        ciphDecrypt([encryptedSavedStateArr[index]], state.masterkey).then(str => {        
+            if(index && (index % 500 === 0)) {
+                setTimeout(() => {
+                    setLocalState({ str: localState.str + str, index: localState.index + 1 });
+                }, 1);
+            } else {
+                setLocalState({ str: localState.str + str, index: localState.index + 1 });
+            }
         }, () => {
             alert('Decrypt failed');
         });
+    }
+
+    useEffect(() => {
+        setTimeout(()=>{
+            decrypt(localState.index);
+        }, 20)
     }, []);
+
+    useEffect(()=>{
+        if(encryptedSavedStateArr.length === localState.index) {
+            dispatch( { type: 'LOAD_STATE_COMPLETE', str: localState.str } );
+        } else {
+            decrypt(localState.index);
+        }
+    });
 
     return (
         <div className='DecryptingForm'>
-            <h3>Decrypting stored data. <br></br> Please wait...</h3>
+            <p>{localState.str}</p>
         </div>
     );
   };
